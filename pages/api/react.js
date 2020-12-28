4023f4ae3a252e539db8c65673aa330c9a127438
yellowbee <709341218@qq.com>
2020-12-28 17:28:54 +0800

    refactor: edit code

--- pages/api/react.js
+++ pages/api/react.js
@@ -1,26 +1,25 @@
-const path = require('path')
-const { readFileSync } = require('fs')
-const React = require('react')
-const { pipeToNodeWritable } = require('react-server-dom-webpack/writer');
-const ReactApp = require('../server/App').default;
-
+import path from 'path'
+import { readFileSync } from 'fs'
+import React from 'react'
+import { pipeToNodeWritable } from 'react-server-dom-webpack/writer'
+import ReactApp from '../../server/components/App'
 
 async function renderReactTree(res, props) {
   const manifest = readFileSync(
     path.resolve('./.next/react-client-manifest.json'),
     'utf8'
-  );
-  console.log(manifest)
-  const moduleMap = JSON.parse(manifest);
-  pipeToNodeWritable(React.createElement(ReactApp, props), res, moduleMap);
+  )
+
+  const moduleMap = JSON.parse(manifest)
+  pipeToNodeWritable(React.createElement(ReactApp, props), res, moduleMap)
 }
 
 function sendResponse(req, res, redirectToId) {
   renderReactTree(res, {
     selectedId: 1,
-  });
+  })
 }
 
 export default function handler(req, res) {
-  sendResponse(req, res, null);
+  sendResponse(req, res, null)
 }
---
+++ server/components/App.js
@@ -0,0 +1,9 @@
+export default function App({ selectedId, isEditing, searchText }) {
+  return (
+    <div className="main">
+      <div
+        style={{ backgroundColor: 'red', width: '400px', height: '300px' }}
+      ></div>
+    </div>
+  )
+}
