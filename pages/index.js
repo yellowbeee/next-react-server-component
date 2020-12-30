712b79ff538fb7039fd0a4af309174eec967de37
李毅 <liyi@zhongshianhong.com>
2020-12-30 19:48:47 +0800

    ''

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import useServerComponent from '../hooks/useServerComponent'
+import TodoList from './todoList/todoList'
 
 function Home() {
   const App = useServerComponent('/api/app')
-  return <div>{App}</div>
+  return (
+    <div>
+      {/* {App} */}
+      <TodoList></TodoList>
+    </div>
+  )
 }
-
 export default Home
---
+++ server/components/App.js
@@ -0,0 +1,3 @@
+export default function App({selectedId, isEditing, searchText}) {
+  return <div className="main"></div>
+}
