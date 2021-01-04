b33ed9c2d36d6099a435229d6068ceca8124e482
李毅 <liyi@zhongshianhong.com>
2021-01-04 18:29:50 +0800

    '拆分Todolist'

--- components/TodoList/index.client.js
+++ components/TodoList/index.client.js
@@ -3,21 +3,19 @@ import style from './index.module.css'
 
 function TodoList({list, changeChecked, handleDel}) {
   return (
-    list.lenth && (
-      <ul className={style.todoUl}>
-        {list.map((item, index) => {
-          return (
-            <li key={index}>
-              <span
-                className={`${style.checkBox} ${item.status && style.checked}`}
-                onClick={() => changeChecked(index)}></span>
-              <span className={style.text}>{item.text}</span>
-              <span className={style.delButton} onClick={() => handleDel(index)}></span>
-            </li>
-          )
-        })}
-      </ul>
-    )
+    <ul className={style.todoUl}>
+      {list.map((item, index) => {
+        return (
+          <li key={index}>
+            <span
+              className={`${style.checkBox} ${item.status && style.checked}`}
+              onClick={() => changeChecked(index)}></span>
+            <span className={style.text}>{item.text}</span>
+            <span className={style.delButton} onClick={() => handleDel(index)}></span>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
